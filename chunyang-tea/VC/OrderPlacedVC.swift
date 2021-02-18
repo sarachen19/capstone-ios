1c3b7ccfa45517024b9f954b6235fb2fda579bc8
Brad <55197671+BradGQH@users.noreply.github.com>
2021-02-19 01:50:59 +0800

    Add new VC
    1. Adjust the cart VC. Change push to present modally.
    2. Temporarily remove connection from detailVC to Cart.
    3. Add a OrderPlaced VC connected to the 'Confirm' button in Cart VC.

---
+++ chunyang-tea/VC/OrderPlacedVC.swift
@@ -0,0 +1,31 @@
+//
+//  OrderPlacedVC.swift
+//  chunyang-tea
+//
+//  Created by Brad Guo on 2021-02-19.
+//
+
+import UIKit
+
+class OrderPlacedVC: UIViewController {
+
+    @IBOutlet weak var CircleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
