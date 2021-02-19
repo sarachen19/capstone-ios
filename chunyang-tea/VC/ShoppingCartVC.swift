87b65418d96844118316d7479be8b092ea819f9a
Brad <55197671+BradGQH@users.noreply.github.com>
2021-02-20 00:45:19 +0800

    Testing the logic to write to Firebase
    Successfully write to orderList when user click CONFIRM

--- chunyang-tea/VC/ShoppingCartVC.swift
+++ chunyang-tea/VC/ShoppingCartVC.swift
@@ -15,14 +15,39 @@ class ShoppingCartVC : UIViewController, UITableViewDelegate, UITableViewDataSou
     var shoppingList : [CartItem]?
     @IBOutlet weak var tableView: UITableView!
     
+    // Firebase reference
+    let ref = Database.database().reference()
+    
+    
     // ⚠️Action when user click confirm
     // 1. Add all drinks inside cart into Database
     // 2. Clear the cart
     // 3. Close this VC & Navigate to OrderPlaceVC
     @IBAction func confirmAction(_ sender: Any) {
         
-        // 1. Add all drinks inside cart into Database
+
+    // ⚠️Make sure the confirmButton is available only if the cart is not empty
         
+        // 1. Add all drinks inside cart into Database
+        ref.child("orderList").childByAutoId().setValue([
+                                                            "Drink1":[
+                                                                        "Ice":"Less",
+                                                                        "Sugar":"Normal"
+                                                                     ],
+                                                            "Drink2":[
+                                                                        "Ice":"Less",
+                                                                        "Sugar":"Normal"
+                                                                     ],
+                                                            "orderTime":"2021-02-19 22:40:01",
+                                                            "totalPrice":12
+                                                        ])
+        let length = shoppingList?.count
+        if length! > 0 {
+            for index in 0...(length!-1){
+                print(shoppingList![index].tea?.name_en)
+            }
+        }
+
         
         // 3. Close this VC & Navigate to OrderPlaceVC
         self.navigationController!.pushViewController(self.storyboard!.instantiateViewController(withIdentifier: "OrderPlacedVC") as UIViewController, animated: true)
@@ -45,15 +70,15 @@ class ShoppingCartVC : UIViewController, UITableViewDelegate, UITableViewDataSou
         self.shoppingList = SharedData.shoppingList
         print(UserDefaults.standard.string(forKey: "userEmail"))
         
-        let ref = Database.database().reference()
-//        ref.child("userCredits/userID").setValue("2@gmail.com")
-        let orderID:String = "test12345"
-//        ref.child("userCredits/2@gmail,com/orderID").setValue(orderID)
-        ref.child("userCredits/2@gmail,com").child(orderID).setValue([
-                                                               "creditBefore":70,
-                                                               "creditAfter":80,
-                                                               "creditChange":10
-                                                              ])
+//        let ref = Database.database().reference()
+////        ref.child("userCredits/userID").setValue("2@gmail.com")
+//        let orderID:String = "test12345"
+////        ref.child("userCredits/2@gmail,com/orderID").setValue(orderID)
+//        ref.child("userCredits/2@gmail,com").child(orderID).setValue([
+//                                                               "creditBefore":70,
+//                                                               "creditAfter":80,
+//                                                               "creditChange":10
+//                                                              ])
 
     }
 
