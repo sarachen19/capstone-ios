6065fbe546ad89f388adc5d1290d91f1df65189c
Brad <55197671+BradGQH@users.noreply.github.com>
2021-02-20 20:56:04 +0800

    Remove table view in orderPlacedVC
    Remove unnecessary table view design in orderPlacedVC

--- chunyang-tea/VC/OrderPlacedVC.swift
+++ chunyang-tea/VC/OrderPlacedVC.swift
@@ -10,29 +10,15 @@ import FirebaseUI
 import FirebaseCore
 import FirebaseDatabase
 
-class OrderPlacedVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
+class OrderPlacedVC: UIViewController{
 
     // Create orderID to display info related to that order
     var orderID:String = ""
     // Firebase reference
     let ref = Database.database().reference()
-    // The object to help stop the listener?
-    var databaseHandle:DatabaseHandle?
-    // Create a dictionary to store drink name and status
-    var drinkListDict:NSDictionary?
     
     @IBOutlet weak var qNumber: UILabel!
     @IBOutlet weak var amountNotice: UITextView!
-    @IBOutlet weak var drinksTableView: UITableView!
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.drinkListDict!.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        return cell
-    }
     
     // When user click DONE, close this view and return to Menu VC.
     @IBAction func toMenuVC(_ sender: Any) {
@@ -46,17 +32,8 @@ class OrderPlacedVC: UIViewController,UITableViewDelegate, UITableViewDataSource
 
         // Hide navigation bar
         self.navigationController?.setNavigationBarHidden(true, animated: false)
-        // Set table view background color
-        self.drinksTableView.backgroundColor = UIColor.init(named: "DeepBlueColor")
         
-        // Get order info from firebase and set a listener monitoring any changes
-
-            // Retrieve order info and put a listener
-        databaseHandle = ref.child("orderList/\(orderID)/drinks").observe(.childChanged) { (snapshot) in
-            //Code to execute when a child is changed
-            let drinkListDict = snapshot.value as? NSDictionary
-            print(drinkListDict?.count)
-        }
+        // Get order info from firebase 
         
         // Do any additional setup after loading the view.
     }
