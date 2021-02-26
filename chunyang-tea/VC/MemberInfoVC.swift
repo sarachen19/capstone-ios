6d11d03bb469adf31ecaf6cf6bc6c6f0e7fdd0e6
Brad <55197671+BradGQH@users.noreply.github.com>
2021-02-26 20:59:02 +0800

    Arrange 2 new VC
    One for membership info.
    Another for history, navigated from membershipInfoVC

---
+++ chunyang-tea/VC/MemberInfoVC.swift
@@ -0,0 +1,29 @@
+//
+//  MemberInfoVC.swift
+//  chunyang-tea
+//
+//  Created by Brad Guo on 2021-02-26.
+//
+
+import UIKit
+
+class MemberInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
